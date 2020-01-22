ab72c479aa3cce23522ff5972fc45eceeb797207
Unknown <timamusic@yandex.ru>
2020-01-22 12:29:57 -0800

    add slider

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.corusel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.png" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.png" alt=""></button>'
+    });
+  });
