5e7f4de4a55eb6b5cd21ea2af85157f7aadbeea4
Unknown <timamusic@yandex.ru>
2020-02-16 09:50:29 -0800

    win section catalog

--- src/js/script.js
+++ src/js/script.js
@@ -5,4 +5,24 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.png" alt=""></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.png" alt=""></button>'
     });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+
+      function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog__item__price').eq(i).toggleClass('catalog__item__price_active');
+                $('.catalog__item__list').eq(i).toggleClass('catalog__item__list_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog__item__link');
+    toggleSlide('.catalog__item__back');
+
   });
